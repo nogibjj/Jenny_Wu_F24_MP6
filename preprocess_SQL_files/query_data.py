01567c9ff76ad597d938bb7ce0e5f053c3069e96
Jenny <wujenny214@gmail.com>
2024-10-20 17:05:41 -0400

    change to query connection

--- preprocess_SQL_files/query_data.py
+++ preprocess_SQL_files/query_data.py
@@ -9,14 +9,20 @@ def general_query(query):
     """runs a query a user inputs"""
 
     load_dotenv()
-    connection = sql.connect(
-        server_hostname=os.getenv("SERVER_HOSTNAME"),
-        http_path=os.getenv("HTTP_PATH"),
-        access_token=os.getenv("DATABRICKS_KEY"),
-    )
-    c = connection.cursor()
-    c.execute(query)
-    result = c.fetchall()
-    connection.commit()
-    c.close()
+    server_hostname_test = os.getenv("SERVER_HOSTNAME")
+    http_path_test = os.getenv("DATABRICKS_HTTPPATH")
+    access_token_test = os.getenv("DATABRICKS_KEY")
+
+    # Connect to DataBricks database
+    with sql.connect(
+        server_hostname=server_hostname_test,
+        http_path=http_path_test,
+        access_token=access_token_test,
+    ) as connection:
+
+        with connection.cursor() as c:
+            c.execute(query)
+            result = c.fetchall()
+            connection.commit()
+            c.close()
     return result
